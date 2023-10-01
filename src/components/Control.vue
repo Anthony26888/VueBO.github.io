ee20d32ca6759abea78b7f21cb83732d93149e33
Anthoy Nguyen <haidang11111995@gmail.com>
2023-10-01 23:12:35 +0700

    update

--- src/components/Control.vue
+++ src/components/Control.vue
@@ -2,26 +2,47 @@
     <div class="container">
         <div class="group-input">
             <p>Giá trị</p>
-            <div class="d-flex flex-row justify-content-center gap-1">
-                <button type="button" class="btn btn-outline-secondary">+</button>
-                <input type="text">
-                <button type="button" class="btn btn-outline-secondary">-</button>
+            <div class="d-flex flex-row justify-content-center gap-2">
+                <button type="button" class="btn btn-outline-secondary" v-on:click="Div">-</button>
+                <input type="num" class="text-end" v-model="NumValue">
+                <button type="button" class="btn btn-outline-secondary" v-on:click="Plus">+</button>    
+                            
             </div>
-            <div class="d-flex flex-row justify-content-between flex-wrap gap-1 mt-2">
-                <button type="button" class="btn btn-outline-secondary value p-2">+5</button>
-                <button type="button" class="btn btn-outline-secondary value">+10</button>
-                <button type="button" class="btn btn-outline-secondary value">+20</button>
-                <button type="button" class="btn btn-outline-secondary value">+50</button>
-                <button type="button" class="btn btn-outline-secondary value">+100</button>
-                <button type="button" class="btn btn-outline-secondary value">All</button>
-                
+            <div class=" d-flex flex-column mt-3 ms-5 gap-2">
+                <div class="row gap-2">
+                    <button type="button" v-on:click="getValue" class="btn btn-outline-secondary value">+{{inputData[0]}}</button>
+                    <button type="button" class="btn btn-outline-secondary value">+{{inputData[1]}}</button>
+                    <button type="button" class="btn btn-outline-secondary value">+{{inputData[2]}}</button>
+                </div>  
+                <div class="row gap-2">
+                    <button type="button" class="btn btn-outline-secondary value">+{{inputData[3]}}</button>
+                    <button type="button" class="btn btn-outline-secondary value">+{{inputData[4]}}</button>
+                    <button type="button" class="btn btn-outline-secondary value">{{inputData[5]}}</button>      
+                </div> 
             </div>
+            
         </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            NumValue:0,
+            inputData: [5, 10, 20, 50, 100, 'All' ]
+        }
+    },
+    methods: {
+        Plus(){
+            this.NumValue++
+        },
+        Div(){            
+            this.NumValue--           
+        },
+        getValue(){
+            this.NumValue = inputData[0]
+        }
+    },
 }
 </script>
 <style scoped>
@@ -30,12 +51,13 @@ export default {
         height: 100vh;
     }
     input{
-        max-width: 90px;
+        max-width: 100px;
     }
     
     .value{
         width: 59px;
         height: 36px;
-        
+        align-items: center;   
     }
+  
 </style>
